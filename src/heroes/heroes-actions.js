379293feb5deae0e9fb377f532a488466c82926f
mudasar187 <ahmmud16@hotmail.com>
2019-12-15 12:34:59 +0100

    more action types

--- src/heroes/heroes-actions.js
+++ src/heroes/heroes-actions.js
@@ -4,6 +4,14 @@ export const FETCH_HEROES_REQUEST = "FETCH_HEROES_REQUEST";
 export const FETCH_HEROES_SUCCESS = "FETCH_HEROES_SUCCESS";
 export const FETCH_HEROES_FAIL = "FETCH_HEROES_FAIL";
 
+export const DELETE_HERO_REQUEST = "DELETE_HERO_REQUEST";
+export const DELETE_HERO_SUCCESS = "DELETE_HERO_SUCCESS";
+export const DELETE_HERO_FAIL = "DELETE_HERO_FAIL";
+
+export const ADD_HERO_REQUEST = "ADD_HERO_REQUEST";
+export const ADD_HERO_SUCCESS = "ADD_HERO_SUCCESS";
+export const ADD_HERO_FAIL = "ADD_HERO_FAIL";
+
 /* 
 action creators for Saga, which uses function generator side effect. 
 Saga will watch these request
@@ -11,4 +19,9 @@ Saga will watch these request
 
 export const fetchHeroes = () => ({
   type: FETCH_HEROES_REQUEST
+});
+
+export const removeHero = id => ({
+  type: DELETE_HERO_REQUEST,
+  payload: id
 });
